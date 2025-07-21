1e83a45115291e102d8254fde0133de3be9a5b64
Sejal Sinha <sejalsinha.is22@bmsce.ac.in>
2025-07-22 01:36:06 +0530

    fixed login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -61,9 +61,6 @@ export default function LoginPage() {
             <div className="space-y-2">
               <div className="flex items-center justify-between">
                 <Label htmlFor="password">Password</Label>
-                <Link href="#" className="text-xs text-primary hover:underline">
-                  Forgot password?
-                </Link>
               </div>
               <Input
                 id="password"
